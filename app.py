0fad5354c73f41f56f49be51ea608421f2dfc3ee
gebarros <gebarrosbio@gmail.com>
2019-09-18 14:53:58 -0300

    login page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 # Create the app object
 app = Flask(__name__)
@@ -8,6 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    return render_template('index.html')
+
+# login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  error = None
+  if request.method == 'POST':
+    if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+      error = 'Invalid Credentials. Please try again.'
+    else:
+      return redirect(url_for('index'))
+  return render_template('login.html', error=error)
 
 
 # Start the server with the 'run()' method
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/gebarros/git-lab/flask-app-temp/venv"
+set -gx VIRTUAL_ENV "/home/gebarros/git-hub/arbioinfo-app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
